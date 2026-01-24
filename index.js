49311630f818bfd0ecb728f972c7af577740bdc6
Kirb-Z <nickolasknowles1920@gmail.com>
2026-01-23 20:30:26 -0500

    Add catch-all route for main URL
    Added a catch-all route to respond to the main URL.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+// This fixes the 404 for the main URL
+app.all("/", (req, res) => {
+  res.status(200).send("Server is alive!");
+});
+
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
